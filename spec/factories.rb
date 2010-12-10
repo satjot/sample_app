a1e86dc5d2dc270dc9399d8a5aec34ae499e0331
Satjot Sawhney <satjot@gmail.com>
2010-12-10 16:38:45 -0500

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Satjot Sawhney"
+  user.email                 "satjot@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
