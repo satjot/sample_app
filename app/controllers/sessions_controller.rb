6e21eaf379b075b48988cc036a049a3d371f47ba
Satjot Sawhney <satjot@gmail.com>
2010-12-14 12:43:08 -0500

    sign in failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
     @title = "Sign in"
+    render 'new'
   end
   
   def create
-    
+    user = User.authenticate(params[:session][:email],
+                             params[:session])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination"
+      @title = "Sign in"
+      render 'new'
+    else
+      # handle signin
+    end
   end
   
   def destroy
-    
   end
 
 end
